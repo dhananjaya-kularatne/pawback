0bbdb38c611359b4466af01f7d0c1db8c6cd26cd
OnelDJ <oneldesilva868@gmail.com>
2026-08-07 12:15:31 +0530

    feat(PAW-14): update UserRepository and create JwtUtil

--- backend/src/main/java/com/pawback/pawback/repository/UserRepository.java
+++ backend/src/main/java/com/pawback/pawback/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.pawback.pawback.repository;
 import com.pawback.pawback.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
